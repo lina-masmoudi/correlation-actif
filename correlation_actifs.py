18f9f81e44ddc4ff5170f9e6405bee9bae137645
lina-masmoudi <lina.masmoudi@icloud.com>
2025-05-19 10:50:28 +0200

    correlation_actifs.py

--- correlation_actifs.py
+++ correlation_actifs.py
@@ -1,25 +1,20 @@
-print ("Projet correlation lancé")
 import yfinance as yf
 import pandas as pd 
-# Liste des tickers (symboles boursiers)
 tickers = ["GLD","USO","^GSPC","^NDX"]
-# Télécharger les données de prix (prix de clôture)
+# Télécharger les données de prix
 data = yf.download(tickers, start= "2025-01-01", end="2025-05-01")
 close_prices=data["Close"]
 print(data.head())
 alias= {"GLD": "Or", "USO": "Pétrole", "^GSPC": "S&P500", "^NDX": "Nasdaq 100"}
 close_prices.rename(columns=alias, inplace=True)
-# Calculer la matrice de corrélation
+# Matrice de corrélation
 correlation_matrix = close_prices.corr()
-# Afficher la matrice de corrélation
 print ("\nMatrice de corrélation:")
 print(correlation_matrix)
 import seaborn as sns
 import matplotlib.pyplot as plt
-# Création de la heatmap
 plt.figure(figsize=(10,8))
 sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5, annot_kws={"size":8})
 plt.tight_layout
-# Titre et affichage
 plt.title ("Matrice de corrélation entre actifs")
 plt.show()
